4c18be465f7a3ee129c8b8df1c222fcb4e01ce08
Maharta Pamuji <gedemaharta.dev@gmail.com>
2025-06-19 21:56:21 +0700

    Select component done. v1

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
@@ -7,12 +7,54 @@ import { getDisplayedValue } from './Select.helpers';
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
+  const [width, setWidth] = useState(0);
+
+  const ref = useRef(null);
+
+  useLayoutEffect(() => {
+    if (ref?.current) {
+      setWidth(ref.current.offsetWidth);
+    }
+  }, [displayedValue]);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <div>
+      <CustomSelect ref={ref}>
+        <CurrentValue>{displayedValue}</CurrentValue>
+        <Icon id="chevron-down" />
+      </CustomSelect>
+      <Wrapper
+        style={{ '--width': width + 'px' }}
+        value={value}
+        onChange={onChange}
+        aria-label={label}
+      >
+        {children}
+      </Wrapper>
+    </div>
   );
 };
+
+const Wrapper = styled.select`
+  opacity: 0;
+  height: 43px;
+  position: absolute;
+  width: var(--width);
+`;
+
+const CustomSelect = styled.div`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  background-color: ${COLORS.transparentGray15};
+  height: 43px;
+  border-radius: 8px;
+  padding: 12px 16px;
+`;
+
+const CurrentValue = styled.p`
+  margin-right: 24px;
+  color: ${COLORS.gray700};
+`;
 
 export default Select;
